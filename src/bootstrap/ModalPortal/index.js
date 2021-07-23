ebada44d2c0a251edd1b3c7828248a74c1c750ee
Tomáš Havlas <havlas@bbxnet.sk>
2021-07-23 13:39:02 +0200

    refactor: remove unused import

--- src/bootstrap/ModalPortal/index.js
+++ src/bootstrap/ModalPortal/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import { createPortal } from 'react-dom'
 import useBoolState from '../../hook/useBoolState'
 import createBackdropElement from './createBackdropElement'
