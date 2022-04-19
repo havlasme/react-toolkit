5f1a08b960c39e5de49d4891d136b7e07cce2d0a
Tomáš Havlas <havlas@bbxnet.sk>
2022-04-19 14:18:03 +0200

    feat(isOK): add HTTP isOK util

---
+++ src/http/isOK.js
@@ -0,0 +1,22 @@
+import HttpCode from './HttpCode'
+
+/**
+ * Check if the status code is in 2xx.
+ *
+ * @param {number} code
+ * @return {boolean}
+ */
+const isOK = function (code) {
+    return code === HttpCode.OK
+        || code === HttpCode.CREATED
+        || code === HttpCode.ACCEPTED
+        || code === HttpCode.NON_AUTHORITATIVE_INFORMATION
+        || code === HttpCode.NO_CONTENT
+        || code === HttpCode.RESET_CONTENT
+        || code === HttpCode.PARTIAL_CONTENT
+        || code === HttpCode.MULTI_STATUS
+        || code === HttpCode.ALREADY_REPORTED
+        || code === HttpCode.IM_USED
+}
+
+export default isOK
