93adf98ba26fa32173583669400132e95e26510b
Tomáš Havlas <havlas@bbxnet.sk>
2022-06-06 15:50:19 +0200

    feat(useFinalCallback): add blurActiveElement option

--- src/hook/useFinalCallback.js
+++ src/hook/useFinalCallback.js
@@ -6,13 +6,15 @@ import { useCallback } from 'react'
  * @param {function} callback
  * @param {*} dependency
  * @param {Object} option
+ * @param {boolean} option.blurActiveElement
  * @param {boolean} option.preventDefault
  * @return {function}
  */
-const useFinalCallback = function (callback, dependency = [], { preventDefault = false } = {}) {
+const useFinalCallback = function (callback, dependency = [], { blurActiveElement = false, preventDefault = false } = {}) {
     return useCallback(function (event, ...rest) {
         event?.stopPropagation()
         preventDefault && event?.preventDefault()
+        blurActiveElement && document.activeElement?.blur()
         callback && callback(event, ...rest)
     }, [...dependency, preventDefault])
 }
