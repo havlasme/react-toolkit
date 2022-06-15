7f806cfb222dc27f909989d7b09b86be8954e859
Tomáš Havlas <havlas@bbxnet.sk>
2022-06-15 16:34:07 +0200

    feat(focusEventTarget): add focusEventTarget utility

---
+++ src/util/focusEventTarget.js
@@ -0,0 +1,10 @@
+/**
+ * Focus the event current target element.
+ *
+ * @param {*} event
+ */
+const focusEventTarget = function (event) {
+    event.currentTarget.focus()
+}
+
+export default focusEventTarget
