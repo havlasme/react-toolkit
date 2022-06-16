c82303fbd830a36aea7c9cc7375c1e191c7e0c71
Tomáš Havlas <tomas@havlas.me>
2022-06-16 21:04:20 +0200

    feat(useProxyCallback): payload can be callback, proxy result of given callback executed with event as argument

--- src/hook/useProxyCallback.js
+++ src/hook/useProxyCallback.js
@@ -1,20 +1,21 @@
 import { useCallback } from 'react'
+import runCallable from '../util/runCallable'
 
 /**
  * The useProxyCallback hook.
  *
  * @param {function} callback
- * @param {*} data
+ * @param {*} payload
  * @param {*} dependency
  * @param {Object} option
  * @param {boolean} option.proxyEvent
  * @return {function}
  */
-const useProxyCallback = function (callback, data, dependency = [], { proxyEvent = true } = {}) {
+const useProxyCallback = function (callback, payload, dependency = [], { proxyEvent = true } = {}) {
     return useCallback(function (event, ...rest) {
-        if (proxyEvent) return callback(event, data, ...rest)
-        return callback(data, ...rest)
-    }, [...dependency, data, proxyEvent])
+        if (proxyEvent) return callback(event, runCallable(payload, event), ...rest)
+        return callback(runCallable(payload, event), ...rest)
+    }, [...dependency, payload, proxyEvent])
 }
 
 export default useProxyCallback
