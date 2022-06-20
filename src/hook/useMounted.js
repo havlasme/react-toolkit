87eb44dc16d05e7ae091b89f86abc8c4e63b8027
Tomáš Havlas <havlas@bbxnet.sk>
2022-06-20 15:36:32 +0200

    feat(useMounted): add callback support

--- src/hook/useMounted.js
+++ src/hook/useMounted.js
@@ -1,15 +1,21 @@
 import { useEffect, useState } from 'react'
+import runEventCallback from '../util/runEventCallback'
 
 /**
  * The useMounted hook.
  *
+ * @param {function} [callback=null]  a callback executed when the component is mounted
  * @return {boolean}
  */
-const useMounted = function () {
+const useMounted = function (callback = null) {
     const [mounted, set] = useState(false)
 
     useEffect(function () {
+        // run the callback
+        runEventCallback(callback)
+        // update the state to true
         set(true)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     return mounted
