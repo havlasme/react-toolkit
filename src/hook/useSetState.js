575d16cbb599def52ecaa0e162e51c4379fdb54a
Tomáš Havlas <tomas@havlas.me>
2022-06-21 20:47:02 +0200

    docs(useSetState): add jsdoc example [skip ci]

--- src/hook/useSetState.js
+++ src/hook/useSetState.js
@@ -4,11 +4,23 @@ import { useCallback, useState } from 'react'
 /**
  * The useSetState hook.
  *
- * @param {*} initialValue
- * @return {[*, function]}
+ * @param {Object} initialState  the initial state
+ * @return {[Object, function]}
+ * @example
+ *
+ * const Component = function () {
+ *   const [state, set] = useSetState({ prop1: 'value1', prop2: 'value2' })
+ *
+ *   useEffect(function () {
+ *       set({ prop2: 'new-value' })
+ *       // the state should contain { prop1: 'value1', prop2: 'new-value' }
+ *   }, [])
+ *
+ *   return null
+ * }
  */
-const useSetState = function (initialValue) {
-    const [state, set] = useState(initialValue)
+const useSetState = function (initialState) {
+    const [state, set] = useState(initialState)
 
     const setState = useCallback(function (state) {
         if (is(Function, state)) {
