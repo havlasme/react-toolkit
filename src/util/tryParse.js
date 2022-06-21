bef6ef6a234e2ffaa99a3751ffaf9a0e7eda7b82
Tomáš Havlas <tomas@havlas.me>
2022-06-21 21:07:50 +0200

    docs(tryParse): add jsdoc example [skip ci]

--- src/util/tryParse.js
+++ src/util/tryParse.js
@@ -1,8 +1,14 @@
 /**
  * Try to parse JSON from the value, return original value on failure.
  *
- * @param {*} value
+ * @param {*} value  the value
  * @return {*}
+ * @example
+ *
+ * const response = null
+ *
+ * console.log(tryParse(response))
+ * // this should log null
  */
 const tryParse = function (value) {
     try {
