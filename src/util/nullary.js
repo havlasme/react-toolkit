2e24223dd9012874e9f663b313c7675be9856747
Tomáš Havlas <tomas@havlas.me>
2022-06-21 21:16:48 +0200

    docs(nullary): add jsdoc comment [skip ci]

--- src/util/nullary.js
+++ src/util/nullary.js
@@ -1,3 +1,9 @@
 import { nAry } from 'ramda'
 
+/**
+ * Wrap a function of any arity in a function that dismisses all arguments.
+ *
+ * @param {function} callback  a function
+ * @return {function}
+ */
 export default nAry(0)
