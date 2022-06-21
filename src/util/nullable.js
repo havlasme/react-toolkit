50292e3609e81b5aef0fac3c988f1007546d5e24
Tomáš Havlas <tomas@havlas.me>
2022-06-21 21:13:10 +0200

    docs(nullable): add jsdoc example [skip ci]

--- src/util/nullable.js
+++ src/util/nullable.js
@@ -1,9 +1,22 @@
 /**
  * Used when passing conditional property to the component.
  *
- * @param {*} value
- * @param {boolean} toNull
+ * @param {*} value  the value
+ * @param {boolean} [toNull=true]  return null when set to true
  * @return {*|null}
+ * @example
+ *
+ * const Component = function ({ disabled }) {
+ *   const onClick = useCallback(function () {
+ *     console.log('click event')
+ *   }, [])
+ *
+ *   return (
+ *     <button type="button" onClick={nullable(onClick, disabled)}>
+ *       ...
+ *     </button>
+ *   )
+ * }
  */
 const nullable = function (value, toNull = true) {
     return toNull ? null : value
