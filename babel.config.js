c57c0dd4fb8a5042e0b805620be1ecaab6f8688b
Tomáš Havlas <tomas@havlas.me>
2023-05-01 20:32:29 +0200

    build(react-toolkit): minify babel config

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,7 @@
 module.exports = {
   'presets': [
-    [
-      '@babel/env',
-      {
-        'targets': '> 0.25%, not dead',
-      },
-    ],
-    '@babel/preset-react',
+    ['@babel/env', {'targets': '> 0.25%, not dead'}],
+    ['@babel/preset-react'],
   ],
   'comments': false,
   'minified': true,
