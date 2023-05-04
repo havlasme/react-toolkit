ec86c74ec64eec741b28815713c8c62b67e92ff3
Tomáš Havlas <havlas@bbxnet.sk>
2023-05-04 15:21:34 +0200

    build(react-toolkit): add jsx extension support

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -9,7 +9,7 @@ export default [
     input: 'lib/index.js',
     plugins: [
       externals(),
-      nodeResolve(),
+      nodeResolve({extensions: ['.js', '.jsx', '.json']}),
       commonjs(),
       terser(),
     ],
