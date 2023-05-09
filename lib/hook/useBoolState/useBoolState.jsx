e7da01ad6681738c1c99c58c5ba0bbaaf116f250
Tomáš Havlas <havlas@bbxnet.sk>
2023-05-09 15:09:24 +0200

    refactor(useBoolState): add code documentation

--- lib/hook/useBoolState/useBoolState.jsx
+++ lib/hook/useBoolState/useBoolState.jsx
@@ -7,14 +7,19 @@ import {useCallback, useState} from 'react'
  * @return {[boolean, function]}
  */
 const useBoolState = function (initialState) {
+  // the state. must be a `boolean` value.
   const [state, setState] = useState(initialState)
 
-  const setBoolState = useCallback(function (next) {
-    setState(function (state) {
-      return typeof next === 'boolean'
-        ? next : !state
-    })
-  }, [setState])
+  // the state update callback.
+  // set the state to boolean value.
+  // toggle the state value when `next` is not set (or non-boolean).
+  const setBoolState = useCallback(
+    function (next) {
+      setState(function (state) {
+        return typeof next === 'boolean'
+          ? next : !state
+      })
+    }, [setState])
 
   return [state, setBoolState]
 }
