a776a4ea600cc0444a2df52cc51c06f26f81ab3f
Tomáš Havlas <havlas@bbxnet.sk>
2023-05-11 10:59:28 +0200

    refactor(useDocumentTitle): cleanup code, and add comment

--- lib/hook/useDocumentTitle/useDocumentTitle.jsx
+++ lib/hook/useDocumentTitle/useDocumentTitle.jsx
@@ -5,32 +5,36 @@ const DEFAULT_OPTION_OBJECT = {
 }
 
 /**
- * The useTitle hook.
+ * The useDocumentTitle hook.
  *
- * @param {string} title  the title.
+ * @param {string|null} title  the title.
  * @param {Object} [option]  the option object.
  * @param {boolean} [option.restoreOnUnmount=true]  restore previous title on unmount.
  */
-const useDocumentTitle = function (title, {restoreOnUnmount} = DEFAULT_OPTION_OBJECT) {
+const useDocumentTitle = function (title, option = DEFAULT_OPTION_OBJECT) {
   if (typeof title !== 'string' && title !== null) {
     throw new TypeError('title must be a string|null.')
   }
+  const {restoreOnUnmount = true} = option
 
+  // the previous title ref. to be restored on unmount.
   const prevTitle = useRef(document.title)
 
-  useLayoutEffect(function () {
-    if (typeof title === 'string') {
-      document.title = title
-    }
-  }, [title])
-
-  useLayoutEffect(function () {
-    if (restoreOnUnmount === true) {
-      return function () {
-        document.title = prevTitle.current
+  useLayoutEffect(
+    function () {
+      if (typeof title === 'string') {
+        document.title = title
       }
-    }
-  }, [])
+    }, [title])
+
+  useLayoutEffect(
+    function () {
+      if (restoreOnUnmount === true) {
+        return function () {
+          document.title = prevTitle.current
+        }
+      }
+    }, [])
 }
 
 export default useDocumentTitle
