82228e1cd76405b9599bdc1a0cc785236f086062
Tomáš Havlas <tomas@havlas.me>
2023-05-22 23:03:04 +0200

    refactor(useTimeout): cleanup code, and update documentation

--- lib/hook/useTimeout/useTimeout.jsx
+++ lib/hook/useTimeout/useTimeout.jsx
@@ -4,19 +4,19 @@ import {useCallback, useLayoutEffect, useRef} from 'react'
  * The useTimeout hook.
  *
  * @param {function} callback  the callback.
- * @param {int|null} [delay=null]  the delay (in ms). do not schedule timeout when `null`
+ * @param {int|null} [delay=null]  the delay (in ms). do not schedule timeout when `null`.
  * @return {function}
  */
 const useTimeout = function (callback, delay = null) {
   // the timeout instance ref.
-  const timeout = useRef(null)
+  const timeoutRef = useRef(null)
 
   // the cancel timeout callback.
   const cancelTimeout = useCallback(
     function () {
-      if (timeout.current !== null) {
-        clearTimeout(timeout.current)
-        timeout.current = null
+      if (timeoutRef.current !== null) {
+        clearTimeout(timeoutRef.current)
+        timeoutRef.current = null
       }
     }, [])
 
@@ -24,7 +24,7 @@ const useTimeout = function (callback, delay = null) {
   useLayoutEffect(
     function () {
       if (delay !== null) {
-        timeout.current = setTimeout(callback, delay)
+        timeoutRef.current = setTimeout(callback, delay)
         return cancelTimeout
       }
     }, [callback, delay])
