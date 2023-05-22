16c23bb5220f4be36d97154c0e0ba9e7e5c02d5f
Tomáš Havlas <havlas@bbxnet.sk>
2023-05-22 16:14:02 +0200

    docs(storybook): add mount / unmunt button to the HookTestBedControl component

--- .storybook/component/HookTestBedControl.jsx
+++ .storybook/component/HookTestBedControl.jsx
@@ -9,10 +9,11 @@ import {useState} from 'react'
 const HookTestBedControl = function ({children}) {
   const [key, setKey] = useState(1)
   const [rerender, setRerender] = useState(1)
+  const [mount, setMount] = useState(true)
 
   return (
     <div key={key}>
-      {children}
+      {mount ? children : null}
 
       <div className="mt-4 pt-4 space-x-2 border-t">
         <button onClick={() => setRerender(x => x + 1)} className="py-1.5 px-6 text-neutral-600 font-medium border rounded-md">
@@ -21,6 +22,10 @@ const HookTestBedControl = function ({children}) {
 
         <button onClick={() => setKey(x => x + 1)} className="py-1.5 px-6 text-neutral-600 font-medium border rounded-md">
           Remount hook ({key})
+        </button>
+
+        <button onClick={() => setMount(!mount)} className="py-1.5 px-6 text-neutral-600 font-medium border rounded-md">
+          {mount ? 'Unmount' : 'Mount'} hook
         </button>
       </div>
     </div>
