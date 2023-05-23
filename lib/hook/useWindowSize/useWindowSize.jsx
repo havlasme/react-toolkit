9e78350cb9363a758b37a390577bc754f578e34e
Tomáš Havlas <havlas@bbxnet.sk>
2023-05-23 11:45:18 +0200

    feat(useWindowSize): add resize event callback support

--- lib/hook/useWindowSize/useWindowSize.jsx
+++ lib/hook/useWindowSize/useWindowSize.jsx
@@ -1,24 +1,28 @@
 import {equals} from 'ramda'
-import {useMemo, useSyncExternalStore} from 'react'
+import {useLayoutEffect, useMemo, useSyncExternalStore} from 'react'
 
 const getSnapshot = function () {
   return {height: window.innerHeight, width: window.innerWidth}
 }
 
 const subscribe = function (callback) {
-  window.addEventListener('resize', callback)
+  const onEvent = function () {
+    return callback(getSnapshot())
+  }
+  window.addEventListener('resize', onEvent)
 
   return function () {
-    window.removeEventListener('resize', callback)
+    window.removeEventListener('resize', onEvent)
   }
 }
 
 /**
  * The useWindowSize hook.
  *
+ * @param {function} [callback=null]  the resize event callback.
  * @return {{width : number, height : number}}
  */
-const useWindowSize = function () {
+const useWindowSize = function (callback = null) {
   const getMemoSnapshot = useMemo(function () {
     let memo = null
 
@@ -31,6 +35,13 @@ const useWindowSize = function () {
       return memo
     }
   }, [])
+
+  useLayoutEffect(
+    function () {
+      if (typeof callback === 'function') {
+        return subscribe(callback)
+      }
+    }, [callback])
 
   return useSyncExternalStore(subscribe, getMemoSnapshot)
 }
--- lib/hook/useWindowSize/useWindowSize.testbed.jsx
+++ lib/hook/useWindowSize/useWindowSize.testbed.jsx
@@ -1,12 +1,19 @@
+import {useCallback} from 'react'
 import useWindowSize from './useWindowSize'
 
 const UseWindowSizeTestBed = function () {
-  const windowSize = useWindowSize()
+  const windowSize = useWindowSize(
+    useCallback(
+      function (windowSize) {
+        console.log('windowSize', windowSize)
+      }, []))
 
   return (
     <div className="space-y-4">
       <div className="space-y-2">
         current state: <strong>{JSON.stringify(windowSize)}</strong>
+        <br/>
+        <small>@see console.log</small>
       </div>
     </div>
   )
